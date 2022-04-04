5fa7332395f37bfc1aaf536a34492453c73aaff3
rex-roxie <rex.roxie@outlook.com>
2022-04-04 19:44:56 +0300

    add files and style header a bit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,14 @@
+import './Header.css'
+
+function Header() {
+    return(
+        <div className="header">
+            <header>
+                <h1>Welcome to the Archaeology Fair</h1>
+                <button>Watch Video</button>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
