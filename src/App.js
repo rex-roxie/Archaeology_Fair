718a5ddab6ca57449cc7d450abca4dbd0b66949d
rex-roxie <rex.roxie@outlook.com>
2022-04-04 20:03:35 +0300

    get section for vid

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
 import NavBar from './Components/NavBar/NavBar';
+import Video from './Components/Video/Vid';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Header />
+      <Video />
     </div>
   );
 }
---
+++ src/Components/Video/Vid.js
@@ -0,0 +1,11 @@
+import './Vid.css'
+
+function Video() {
+    return(
+        <div className="video">
+            jfaojafoijofajif
+        </div>
+    );
+}
+
+export default Video;
