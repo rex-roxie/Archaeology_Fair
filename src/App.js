cb23ef76a0f21e889977f25b54e494ba0d2f8e0a
rex-roxie <rex.roxie@outlook.com>
2022-04-04 19:58:33 +0300

    make some styles to navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header/Header';
+import NavBar from './Components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Header />
     </div>
   );
---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.css'
+
+function NavBar() {
+    return(
+        <div className="navbar">
+            <nav>
+                <div id='logo'>
+                    <h2>Archaeology Fair</h2>
+                </div>
+                <ul>
+                    <li>Bio</li>
+                    <li>Video</li>
+                    <li>Video</li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
